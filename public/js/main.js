06ff05e227ed6f2cfebc3837bcf51ddfd97ccde3
Jonathan503-jojo <nathanjoh721@.com>
2024-09-18 15:56:56 +0300

    second commit

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,28 @@ const showMenu = (toggleId, navId) =>{
          nav = document.getElementById(navId)
 
    toggle.addEventListener('click', () =>{
-       // Add show-menu class to nav menu
+       // montrer les menus
        nav.classList.toggle('show-menu')
 
-       // Add show-icon to show and hide the menu icon
+       // montrer l'icon
        toggle.classList.toggle('show-icon')
    })
 }
 
 showMenu('nav-toggle','nav-menu')
+
+let lastScrollTop = 0;
+const navbar = document.getElementById('nav');
+
+window.addEventListener('scroll', function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scrolling vers le bas
+        navbar.style.top = "-100px"; // Hauteur de la navbar pour la faire disparaître
+    } else {
+        // Scrolling vers le haut
+        navbar.style.top = "0";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Pour ne pas avoir un nombre négatif
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,28 +10,14 @@
       <!--=============== CSS ===============-->
       <link rel="stylesheet" href="{{ url('css/menu-navbar.css')}}">
 
-      <title>Responsive dropdown menu - Bedimcode</title>
+      <title>MGH</title>
    </head>
    <body>
-    <div class="logo_tariny">
-        <div class="nav__data">
-            <a href="#" class="nav__logo">
-               <i class="ri-planet-line"></i> <select name="" id="" class="language"><option value="ENG">ENG</option><option value="FR">FR</option></select>
-            </a>
-            
-            <div class="nav__toggle" id="nav-toggle">
-               <i class="ri-menu-line nav__burger"></i>
-               <i class="ri-close-line nav__close"></i>
-            </div>
-         </div>
-    </div>
+      <div class="tete">
       <!--=============== HEADER ===============-->
-      <header class="header">
+      <header class="header" id="nav">
          <nav class="nav container">
             <div class="nav__data">
-               <a href="#" class="nav__logo">
-                  <i class="ri-planet-line"></i> MGH
-               </a>
                
                <div class="nav__toggle" id="nav-toggle">
                   <i class="ri-menu-line nav__burger"></i>
@@ -42,116 +28,55 @@
             <!--=============== NAV MENU ===============-->
             <div class="nav__menu" id="nav-menu">
                <ul class="nav__list">
-                <li class="dropdown__item">
-                    <div class="nav__link">
-                       ACCEUIL <i class="ri-arrow-down-s-line dropdown__arrow"></i>
-                    </div>
-                    <ul class="dropdown__menu">
-                          <h1>ujhgsduigh</h1>
-                    </ul>
-                 </li>
-
-                 <ul class="nav__list">
-                    <li class="dropdown__item">
+                  <!--=============== DROPDOWN 1  ===============-->
+                  <li class="dropdown__item">
+                     <div class="nav__link">
+                           A propos <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                     </div>
+                     <ul class="dropdown__menu">
+                           <h3>jvsdjndjnvvn</h3>
+                     </ul>
+                  </li>
+                  <!--=============== DROPDOWN 2  ===============-->
+                     <li class="dropdown__item">
                         <div class="nav__link">
-                           A PROPOS <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                              Services <i class="ri-arrow-down-s-line dropdown__arrow"></i>
                         </div>
                         <ul class="dropdown__menu">
-                            <h1>texte à propos</h1>
+                              <h3>jvsdjndjnvvn</h3>
                         </ul>
                      </li>
-
-                  <!--=============== DROPDOWN 1 ===============-->
+                  <!--=============== DROPDOWN 3  ===============-->
+                     <li class="dropdown__item">
+                        <div class="nav__link">
+                              Rédirection <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                        </div>
+                        <ul class="dropdown__menu">
+                              <h3>jvsdjndjnvvn</h3>
+                        </ul>
+                     </li>
+                  <!--=============== DROPDOWN 4  ===============-->
                   <li class="dropdown__item">
                      <div class="nav__link">
-                        SERVICES <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                           Partenaires <i class="ri-arrow-down-s-line dropdown__arrow"></i>
                      </div>
-
                      <ul class="dropdown__menu">
-                        <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-pie-chart-line"></i> Overview
-                           </a>                          
-                        </li>
-
-                        <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-arrow-up-down-line"></i> Transactions
-                           </a>
-                        </li>
-
-                        <!--=============== DROPDOWN SUBMENU ===============-->
-                        <li class="dropdown__subitem">
-                           <div class="dropdown__link">
-                              <i class="ri-bar-chart-line"></i> Reports <i class="ri-add-line dropdown__add"></i>
-                           </div>
-
-                           <ul class="dropdown__submenu">
-                              <li>
-                                 <a href="#" class="dropdown__sublink">
-                                    <i class="ri-file-list-line"></i> Documents
-                                 </a>
-                              </li>
-      
-                              <li>
-                                 <a href="#" class="dropdown__sublink">
-                                    <i class="ri-cash-line"></i> Payments
-                                 </a>
-                              </li>
-      
-                              <li>
-                                 <a href="#" class="dropdown__sublink">
-                                    <i class="ri-refund-2-line"></i> Refunds
-                                 </a>
-                              </li>
-                           </ul>
-                        </li>
-                     </ul>
-                  </li>
-                  
-                  <li class="dropdown__item">
-                    <div class="nav__link">
-                       PARTENAIRE <i class="ri-arrow-down-s-line dropdown__arrow"></i>
-                    </div>
-                    <ul class="dropdown__menu">
-                        <h1>texte partenaire</h1>
-                    </ul>
-                 </li>
-
-                  <!--=============== DROPDOWN 2 ===============-->
-                  <li class="dropdown__item">
-                     <div class="nav__link">
-                        REDIRECTION <i class="ri-arrow-down-s-line dropdown__arrow"></i>
-                     </div>
-
-                     <ul class="dropdown__menu">
-                        <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-user-line"></i> Profiles
-                           </a>                          
-                        </li>
-
-                        <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-lock-line"></i> Accounts
-                           </a>
-                        </li>
-
-                        <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-message-3-line"></i> Messages
-                           </a>
-                        </li>
+                           <h3>jvsdjndjnvvn</h3>
                      </ul>
                   </li>
 
-                  <li><a href="#" class="nav__link">CONTACT</a></li>
+                  <li><a href="#" class="nav__link">Careers</a></li>
                </ul>
             </div>
+            <img src="img/logo MGH.jpg" class="image">
          </nav>
       </header>
 
       <!--=============== MAIN JS ===============-->
-      <script src="{{ url('assets/js/main.js')}}"></script>
+      <script src="{{ url('js/main.js')}}"></script>
+   </div>
    </body>
+   <div class="content">
+
+   </div>
 </html>
