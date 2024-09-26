0623222613b375923146eefce83cda85f8683e99
Jonathan503-jojo <nathanjoh721@.com>
2024-09-26 16:28:25 +0300

    04 : 28

--- resources/views/services-aux-passagers.blade.php
+++ resources/views/services-aux-passagers.blade.php
@@ -1,17 +1,36 @@
 @extends('base')
 
 @Section('content')
-<div class="text-image-container">
+<div class="text-image-container-1">
     <!-- Texte à gauche -->
     <div class="text-content">
+        <h1>Services de sécurité</h1>
         <h2>SERVICES AUX PASSAGERS</h2>
-        <li class="liste">
-            <ul>Vérification des documents</ul>
-            <ul>Passenger screening</ul>
-        </li>
+        <ul class="liste">
+            <li>Vérification des documents</li>
+            <li>Contrôle du fret et des bagages</li>
+            <li>Passenger screening</li>
+        </ul>
 
     </div>
+    <!-- Image à droite -->
+    <div class="image-content">
+        <img src="{{ asset('img/services/Service passagers.jpg') }}" alt="services-passagers">
+    </div>
+</div>
 
+<div class="text-image-container-2">
+    <!-- Texte à gauche -->
+    <div class="text-content">
+        <h1>Services de sécurité</h1>
+        <h2>SERVICES AUX PASSAGERS</h2>
+        <ul class="liste">
+            <li>Vérification des documents</li>
+            <li>Contrôle du fret et des bagages</li>
+            <li>Passenger screening</li>
+        </ul>
+
+    </div>
     <!-- Image à droite -->
     <div class="image-content">
         <img src="{{ asset('img/services/Service passagers.jpg') }}" alt="services-passagers">
