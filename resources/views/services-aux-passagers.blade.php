4da8f6072b76523eeb0e73609bb3acdc41bfe6bc
Jonathan503-jojo <nathanjoh721@.com>
2024-09-26 15:46:34 +0300

    03-42

--- resources/views/services-aux-passagers.blade.php
+++ resources/views/services-aux-passagers.blade.php
@@ -5,12 +5,16 @@
     <!-- Texte à gauche -->
     <div class="text-content">
         <h2>SERVICES AUX PASSAGERS</h2>
-        <p>Ceci est un paragraphe explicatif qui s'affiche à gauche. Il peut contenir des informations détaillées sur le sujet.</p>
+        <li class="liste">
+            <ul>Vérification des documents</ul>
+            <ul>Passenger screening</ul>
+        </li>
+
     </div>
 
     <!-- Image à droite -->
     <div class="image-content">
-        <img src="votre-image.jpg" alt="Description de l'image">
+        <img src="{{ asset('img/services/Service passagers.jpg') }}" alt="services-passagers">
     </div>
 </div>
 
